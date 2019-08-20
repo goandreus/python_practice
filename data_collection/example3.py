25d79ee47b607840aefe5a0b914147a1fa983476
Andres Chavez <andreshtml5@gmail.com>
2019-08-20 14:58:44 -0500

    Create example3.py

---
+++ data_collection/example3.py
@@ -0,0 +1,26 @@
+tareas = [ 
+    [6, 'Distribución'],
+    [2, 'Diseño'],
+    [1, 'Concepción'],
+    [7, 'Mantenimiento'],
+    [4, 'Producción'],
+    [3, 'Planificación'],
+    [5, 'Pruebas']
+]
+
+print("==Tareas desordenadas==")
+for tarea in tareas:
+    print(tarea[0], tarea[1])
+
+# Completa el ejercicio aquí
+from collections import deque
+
+tareas.sort()
+
+cola = deque()
+for tarea in tareas:
+    cola.append(tarea[1])
+
+print("\n==Tareas ordenadas==")
+for tarea in cola:
+    print(tarea)
